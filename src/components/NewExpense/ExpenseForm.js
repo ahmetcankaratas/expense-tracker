0336b1ba10c3c6d1bcb4151266094ba167a877aa
lingardium <karatascanahmet@yahoo.com>
2022-07-10 15:44:43 +0000

    awareness about state and stateless(presentation) component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -64,26 +64,26 @@ const ExpenseForm = (props) => {
             onChange={titleChangeHandler}
           />
         </div>
-      </div>
-      <div className="new-expense__control">
-        <label>Amount</label>
-        <input
-          type="number"
-          min="0.01"
-          step="0.01"
-          value={enteredAmount}
-          onChange={amountChangeHandler}
-        />
-      </div>
-      <div className="new-expense__control">
-        <label>Date</label>
-        <input
-          type="date"
-          min="2019-01-01"
-          max="2022-12-31"
-          value={enteredDate}
-          onChange={dateChangeHandler}
-        />
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
